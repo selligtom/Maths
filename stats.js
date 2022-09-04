f904ed91ca8682ffd7e1a078068f08ebaad5f99d
Thomas <selligtom@gmail.com>
2022-09-04 18:12:08 +0200

    Add variance and standard deviation

--- stats.js
+++ stats.js
@@ -14,9 +14,29 @@ function median(table){
     
 }
 
+
+function variance(table){
+
+    let somme = 0;
+    let tableMean = mean(table)
+    
+    for (let index = 0; index < table.length; index++) {
+        somme += Math.pow(table[index] - tableMean, 2)
+    }
+    
+    return somme / table.length
+}
+
+function standardDeviation(table){
+    return Math.pow(variance(table), 0.5)
+}
+
 // Example array 
-let marksTable = [10,15,12.5,10,20,18.25,13,7.5,9.5,17,18,14] 
+let marksTable = [10, 15, 12, 10, 20, 18, 13, 7, 9, 17, 18, 14] 
 
 console.log(`mean : ${mean(marksTable)}`)
 console.log(`median : ${median(marksTable)}`)
+console.log(`variance : ${variance(marksTable)}`)
+console.log(`Standard Deviation : ${standardDeviation(marksTable)}`)
+
 
